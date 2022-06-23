0297d68364bf91e3b3bf8240280e8435e758beb5
Romainv78 <romainvaladon@gmail.com>
2022-06-23 16:36:48 +0200

    user visuel ok

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import Footer from './components/footer/footer';
 import Home from './pages/Home/home';
 import Login from './pages/Login/login';
+import Profil from './pages/Profil/profil';
 import Navbar from './components/navbar/navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,6 +17,7 @@ root.render(
                 <Route path="/" element={<Home />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/user" element={<Profil />} />
             </Routes>
             <Footer />
         </Router>
